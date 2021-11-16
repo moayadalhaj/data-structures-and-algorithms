ec6cb5b185ededc47cfd035cf826426a9bc3c286
moayadalhaj <moayad.alhaj21@gmail.com>
2021-11-16 23:31:27 +0200

    docs: added doc string to the function

--- python/challenges/quick_sort/quick_sort.py
+++ python/challenges/quick_sort/quick_sort.py
@@ -1,5 +1,11 @@
 def QuickSort(list,left,right):
+    """
+    A function that takes list, left=0, right=len(list), as arguments
+    then pass these arguments to Partition function that each loop take a pivot
 
+    input: list, int, int
+    output: sorted list
+    """
     if left < right:
 
         position = Partition(list,left,right)
@@ -9,6 +15,14 @@ def QuickSort(list,left,right):
     return list
 
 def Partition(list,left,right):
+    """
+    Partition function that each loop take a list[right] equal to pivot
+    then check the values that less than this pivot if the condition meet this
+    condition it will pass the index of value that is grater than pivot to Swap function
+
+    input: list, left, right
+    output: index of the value that is grater than pivot
+    """
 
     pivot = list[right]
     low = left - 1
@@ -23,7 +37,12 @@ def Partition(list,left,right):
     return low+1
 
 def Swap(list,i,low):
+    """
+    A function to swap between pivot and the first great value in the list
 
+    input: list, i: int, low: int
+    output: list
+    """
     temp = list[i]
     list[i] = list[low]
     list[low] = temp
